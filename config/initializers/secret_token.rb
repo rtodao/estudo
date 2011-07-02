bf57890628aee80d212b2802883ab704ec4d5534
todao <rtodao@gmail.com>
2011-07-02 02:21:08 -0300

    Done with the estudo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Estudo::Application.config.secret_token = '4d6bee359dcd30fceac9b2e42f5f2a321275a035fb2d1848dec374867472b65e2b1b0e4027e1cc9122ae2fb7c08d464b23b04760ebd282de4339b19df821fe20'
